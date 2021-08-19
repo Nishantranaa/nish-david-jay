50ab85f8cccf7d5fe93eb7e08d4c319357e72655
Davide Patelli <mediadavide@gmail.com>
2021-08-19 14:58:21 +1000

    Staging

--- OurWebSite/footer.php
+++ OurWebSite/footer.php
@@ -1,7 +1,10 @@
-    <footer>
-        <p>ourFooterTest</p>
-    </footer>
 
-<?php get_footer();?>
+<div class="clearfix"></div>
+
+<footer><p> <?php esc_html_e('&copy; Copyright', 'miotema'); ?>  <?php echo date("o");?>  - <?php bloginfo('name'); ?></p></footer>
+
+</div><!--/ .container -->
+
+<?php wp_footer();?>
 </body>
 </html>
